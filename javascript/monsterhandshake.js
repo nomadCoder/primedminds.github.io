ce9e0133d46edb82e1fa75a8103792170f75d8da
drs0611 <devorahrsimon@gmail.com>
2017-05-19 13:35:47 -0400

    Update monsterhandshake.js

--- javascript/monsterhandshake.js
+++ javascript/monsterhandshake.js
@@ -1,19 +1,19 @@
 //Monster Handshake 1.0
 
-$(function() {
-    $("body").click(function(e) {
-        if (e.target.id == "right_arrow") {
-          // current hack
-          window.location.href = "flow_video.html?s=PPV2";
-        } else if (e.target.id == "left_arrow") {
-          window.location.href = "flow_video.html?s=PPV1";
-        } else if (e.target.id == "canvas") { 
-           draw();
-        } else { 
-           //alert("Outside div");
-        }
-    });
-})
+// $(function() {
+//     $("body").click(function(e) {
+//         if (e.target.id == "right_arrow") {
+//           // current hack
+//           window.location.href = "flow_video.html?s=PPV2";
+//         } else if (e.target.id == "left_arrow") {
+//           window.location.href = "flow_video.html?s=PPV1";
+//         } else if (e.target.id == "canvas") { 
+//            draw();
+//         } else { 
+//            //alert("Outside div");
+//         }
+//     });
+// })
 
 
 //helps determine position of monsters
